0e8b5e06403bb432bcd7708317add1d2756ab4bb
NguyenDongNguyen <ndn030802@gmail.com>
2024-01-29 19:00:31 +0700

    design database and connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotnev = require("dotenv");
+const { default: mongoose, connect } = require("mongoose");
 dotnev.config();
 
 const app = express();
@@ -8,6 +9,17 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
     return res.send("Hello world everyone");
 });
+
+mongoose
+    .connect(
+        `mongodb+srv://ndn030802:${process.env.MONGO_DB}@cluster0.escnmx6.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        console.log("Connect Db success!");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.listen(port, () => {
     console.log("Server is running in port: ", +port);
