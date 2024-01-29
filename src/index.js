a235cf471bba9b2c2903e6c4237f814dbc9e6036
NguyenDongNguyen <ndn030802@gmail.com>
2024-01-29 23:20:16 +0700

    phan chia MVC

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const dotnev = require("dotenv");
-const { default: mongoose, connect } = require("mongoose");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+
 dotnev.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-    return res.send("Hello world everyone");
-});
+app.use(bodyParser.json());
+
+routes(app);
 
 mongoose
-    .connect(
-        `mongodb+srv://ndn030802:${process.env.MONGO_DB}@cluster0.escnmx6.mongodb.net/?retryWrites=true&w=majority`
-    )
+    .connect(`${process.env.MONGO_DB}`)
     .then(() => {
         console.log("Connect Db success!");
     })
