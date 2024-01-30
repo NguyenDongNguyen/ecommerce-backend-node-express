fd0da3805990cae5fc90d24168230f65cb4be0ce
NguyenDongNguyen <ndn030802@gmail.com>
2024-01-30 12:45:34 +0700

    tao api dang nhap ket hop json web token

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,9 +1,12 @@
 const User = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+const { genneralAccessToken, genneralRefreshToken } = require("./JwtService");
 
 const createUser = (newUser) => {
     return new Promise(async (resolve, reject) => {
         const { name, email, password, confirmPassword, phone } = newUser;
         try {
+            //check xem email đki đã tồn tại hay chưa
             const checkUser = await User.findOne({
                 email: email,
             });
@@ -13,11 +16,14 @@ const createUser = (newUser) => {
                     message: "The email is exist",
                 });
             }
+
+            // mã hoá password trước khi lưu vào db
+            const hash = bcrypt.hashSync(password, 10);
+
             const createUser = await User.create({
                 name,
                 email,
-                password,
-                confirmPassword,
+                password: hash,
                 phone,
             });
             if (createUser) {
@@ -33,6 +39,54 @@ const createUser = (newUser) => {
     });
 };
 
+const loginUser = (newUser) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, email, password, confirmPassword, phone } = newUser;
+        try {
+            //check xem email đki đã tồn tại hay chưa
+            const checkUser = await User.findOne({
+                email: email,
+            });
+            if (checkUser === null) {
+                resolve({
+                    status: "OK",
+                    message: "The user is not defined",
+                });
+            }
+
+            // so sánh pass user nhập và pass trong db
+            const comparePassword = bcrypt.compareSync(password, checkUser.password);
+
+            if (!comparePassword) {
+                resolve({
+                    status: "OK",
+                    message: "The password or user is incorrect",
+                });
+            }
+
+            const access_token = await genneralAccessToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin,
+            });
+
+            const refresh_token = await genneralRefreshToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin,
+            });
+
+            resolve({
+                status: "OK",
+                message: "SUCCESS",
+                access_token,
+                refresh_token,
+            });
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
 module.exports = {
     createUser,
+    loginUser,
 };
