46278bc7a22703ea36c0c5bf9396e7ea9a8a7848
NguyenDongNguyen <ndn030802@gmail.com>
2024-02-04 01:55:18 +0700

    panigated o phia backend

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -116,14 +116,20 @@ const getDetailProduct = (id) => {
     });
 };
 
-const getAllProduct = () => {
+const getAllProduct = (limit = 8, page = 0) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const allProduct = await Product.find();
+            const totalProduct = await Product.countDocuments();
+            const allProduct = await Product.find()
+                .limit(limit)
+                .skip(page * limit);
             resolve({
                 status: "OK",
                 message: "get all product success",
                 data: allProduct,
+                total: totalProduct,
+                pageCurrent: Number(page + 1),
+                totalPage: Math.ceil(totalProduct / limit),
             });
         } catch (e) {
             reject(e);
