ea807070bbbdb84587aca851985d55a6a9b49ad4
NguyenDongNguyen <ndn030802@gmail.com>
2024-02-03 12:29:45 +0700

    viet api CRUD cho product

---
+++ src/services/ProductService.js
@@ -0,0 +1,140 @@
+const Product = require("../models/ProductModel");
+const bcrypt = require("bcrypt");
+const { genneralAccessToken, genneralRefreshToken } = require("./JwtService");
+
+const createProduct = (newProduct) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, image, type, countInStock, price, rating, description } =
+            newProduct;
+        try {
+            //check xem product đã tồn tại hay chưa
+            const checkProduct = await Product.findOne({
+                name: name,
+            });
+            if (checkProduct !== null) {
+                resolve({
+                    status: "OK",
+                    message: "The name of product is exist",
+                });
+            }
+
+            const createProduct = await Product.create({
+                name,
+                image,
+                type,
+                countInStock,
+                price,
+                rating,
+                description,
+            });
+            if (createProduct) {
+                resolve({
+                    status: "OK",
+                    message: "SUCCESS",
+                    data: createProduct,
+                });
+            }
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+const updateProduct = (id, data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            //check xem product đã tồn tại hay chưa
+            const checkProduct = await Product.findOne({
+                _id: id,
+            });
+            if (checkProduct === null) {
+                resolve({
+                    status: "OK",
+                    message: "The product is not defined",
+                });
+            }
+
+            const updatedProduct = await Product.findByIdAndUpdate(id, data, {
+                new: true,
+            });
+            resolve({
+                status: "OK",
+                message: "SUCCESS",
+                data: updatedProduct,
+            });
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+const deleteProduct = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkProduct = await Product.findOne({
+                _id: id,
+            });
+            if (checkProduct === null) {
+                resolve({
+                    status: "OK",
+                    message: "The product is not defined",
+                });
+            }
+
+            await Product.findByIdAndDelete(id);
+            resolve({
+                status: "OK",
+                message: "Delete product success",
+            });
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+const getDetailProduct = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const product = await Product.findOne({
+                _id: id,
+            });
+            if (product === null) {
+                resolve({
+                    status: "OK",
+                    message: "The product is not defined",
+                });
+            }
+
+            resolve({
+                status: "OK",
+                message: "SUCCESS",
+                data: product,
+            });
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+const getAllProduct = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const allProduct = await Product.find();
+            resolve({
+                status: "OK",
+                message: "get all product success",
+                data: allProduct,
+            });
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+module.exports = {
+    createProduct,
+    updateProduct,
+    getDetailProduct,
+    deleteProduct,
+    getAllProduct,
+};
